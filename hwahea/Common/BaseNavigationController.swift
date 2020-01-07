fe87a25283bfd653bcb828920e9fb7c702da9bd3
jinnify <seungjin429@gmail.com>
2020-01-08 01:52:39 +0900

    add BaseNavigationController

---
+++ hwahea/Common/BaseNavigationController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseNavigationController.swift
+//  hwahea
+//
+//  Created by seungjin on 2020/01/08.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationController: UINavigationController {
+  
+  //MARK:- Constant
+  
+  struct UI {
+    
+  }
+  
+  //MARK:- Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupUI()
+  }
+  
+  //MARK:- Methods
+  
+  private func setupUI() {
+    
+  }
+}
