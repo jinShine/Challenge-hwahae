3a8318d6a8745881463dfaaa9b52df16897997c0
jinnify <seungjin429@gmail.com>
2020-01-08 01:53:18 +0900

    add AppRepository

---
+++ hwahea/Core/Repositories/AppRepository.swift
@@ -0,0 +1,13 @@
+//
+//  AppRepository.swift
+//  hwahea
+//
+//  Created by seungjin on 2020/01/08.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class AppRepository {
+  
+}
