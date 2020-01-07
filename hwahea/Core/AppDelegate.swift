a33e5da8e39c6cad7b822276552676cc5103cb55
jinnify <seungjin429@gmail.com>
2020-01-08 01:54:45 +0900

    Navigator Root 설정

--- hwahea/Core/AppDelegate.swift
+++ hwahea/Core/AppDelegate.swift
@@ -23,9 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   private func setupRoot() {
     let window = UIWindow(frame: UIScreen.main.bounds)
-    window.rootViewController = ViewController()
-    window.backgroundColor = .white
-    window.makeKeyAndVisible()
+    Application.shared.setupInitialScreen(in: window)
     self.window = window
   }
 }
---
+++ hwahea/Modules/Scene/ProductList/ProductListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductListViewModel.swift
+//  hwahea
+//
+//  Created by seungjin on 2020/01/08.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class ProductListViewModel: BaseViewModel {
+  
+}
