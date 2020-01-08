f5dc7d8cb498aa241f133864df96cdacade26861
jinnify <seungjin429@gmail.com>
2020-01-08 23:16:25 +0900

    Font, Color추가

--- hwahea/Utility/Theme/Color.swift
+++ hwahea/Utility/Theme/Color.swift
@@ -14,4 +14,12 @@ struct Color {
     return UIColor(r: 144, g: 19, b: 254, a: 1)
   }
   
+  var blueGrey: UIColor {
+    return UIColor(r: 171, g: 171, b: 196)
+  }
+  
+  var dark: UIColor {
+    return UIColor(r: 20, g: 20, b: 40)
+  }
+  
 }
--- hwahea/Utility/Theme/Font.swift
+++ hwahea/Utility/Theme/Font.swift
@@ -6,8 +6,28 @@
 //  Copyright © 2020 Jinnify. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct Font {
+  
+  //MARK:- Apple SDGothicNeo
+  
+  func appleSDGothicNeoRegular(size: CGFloat) -> UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
+  }
+  
+  func appleSDGothicNeoBold(size: CGFloat) -> UIFont {
+    return UIFont(name: "AppleSDGothicNeo-Bold", size: size) ?? UIFont.systemFont(ofSize: size)
+  }
+  
+  //MARK:- Noto Sans
+  
+  func notoSansBlack(size: CGFloat) -> UIFont {
+    return UIFont(name: "NotoSansCJKkr-Black", size: size) ?? UIFont.systemFont(ofSize: size)
+  }
+  
+  func notoSansBold(size: CGFloat) -> UIFont {
+    return UIFont(name: "NotoSansCJKkr-Bold", size: size) ?? UIFont.systemFont(ofSize: size)
+  }
   
 }
