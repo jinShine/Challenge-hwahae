76344ff66ca6604f3a74ca30c57826146e8387ed
seungjin <seungjin429@naver.com>
2020-01-10 18:18:51 +0900

    Network Service 생성

---
+++ hwahea/Core/Network/NetworkService.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkService.swift
+//  hwahea
+//
+//  Created by Seungjin on 10/01/2020.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import Foundation
+import Moya
+import Alamofire
+
+protocol NetworkServiceType {
+
+  
+}
---
+++ hwahea/Core/Network/hwaheaRouter.swift
@@ -0,0 +1,51 @@
+//
+//  hwaheaRouter.swift
+//  hwahea
+//
+//  Created by Seungjin on 10/01/2020.
+//  Copyright © 2020 Jinnify. All rights reserved.
+//
+
+import Moya
+
+//enum hwaheaRouter {
+//  case productList(skinType: Int, page: Int)
+//}
+//
+//extension hwaheaRouter: TargetType {
+//
+//  var baseURL: URL {
+//    return URL(string: "https://6uqljnm1pb.execute-api.ap-northeast-2.amazonaws.com/prod")!
+//  }
+//
+//  var path: String {
+//    switch self {
+//    case .productList:
+//      return "/products"
+//    }
+//  }
+//
+//  var method: Method {
+//    switch self {
+//    case .productList:
+//      return .get
+//    }
+//  }
+//
+//  var sampleData: Data {
+//    return "data".data(using: String.Encoding.utf8)!
+//  }
+//
+//  var task: Task {
+//    switch self {
+//    case .productList:
+//      case
+//    }
+//  }
+//
+//  var headers: [String : String]? {
+//
+//  }
+//
+//
+//}
